1e1e2915e2a2ae5f7c6f32e6203167a9082b84c0
Rodrigo Avellar <roavellarm@gmail.com>
2020-06-29 23:27:03 -0300

    Add ChatList Page

---
+++ src/pages/ChatList/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Column from 'components/Column'
+
+function ChatList() {
+  return (
+    <>
+      <Column size={12}>
+        <h1>Chat List</h1>
+      </Column>
+    </>
+  )
+}
+
+export default ChatList
