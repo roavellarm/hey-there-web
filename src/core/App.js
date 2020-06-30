e0bf432ff919775e45c39185fbe90f4816f1f0d1
Rodrigo Avellar <roavellarm@gmail.com>
2020-06-29 23:29:29 -0300

    Update App.js

--- src/core/App.js
+++ src/core/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import Router from 'core/Router'
+import Navbar from 'components/Navbar'
 
 function App() {
-  return <h1>Hello!!!</h1>
+  const links = [
+    { name: 'Chat', path: '/' },
+    { name: 'New Contact', path: '/new-contact' },
+    { name: 'Profile', path: '/profile' },
+    { name: 'Login', path: '/login' },
+    { name: 'Register', path: '/register' },
+  ]
+
+  return (
+    <>
+      <Navbar brand="Hey There" links={links} />
+      <Router />
+    </>
+  )
 }
 
 export default App
