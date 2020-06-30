e386dfe10f446d14b7ccf7adec23c2c5b3aadbf8
Rodrigo Avellar <roavellarm@gmail.com>
2020-06-30 06:20:58 -0300

    Add chatList mocks

---
+++ src/mocks/chatList.js
@@ -0,0 +1,51 @@
+import image1 from 'assets/01.jpeg'
+import image2 from 'assets/02.jpeg'
+import image3 from 'assets/03.jpeg'
+import image4 from 'assets/04.jpeg'
+import image5 from 'assets/05.jpeg'
+import image6 from 'assets/06.jpeg'
+
+export const chatList = [
+  {
+    nickname: 'Jessica dos Santos',
+    avatar: image1,
+    msgPreview: 'You: Poxa vida! :(',
+    time: '12:50 PM',
+    newMessages: '5',
+  },
+  {
+    nickname: 'Maria',
+    avatar: image2,
+    msgPreview: 'Maria: E ai gatinho? Tudo bem?',
+    time: '10:03 AM',
+    newMessages: '2',
+  },
+  {
+    nickname: 'Rebeca',
+    avatar: image3,
+    msgPreview: 'You: Poxa vida! :(',
+    time: '09:27 AM',
+    newMessages: '0',
+  },
+  {
+    nickname: 'Gaby',
+    avatar: image4,
+    msgPreview: 'Gaby: Bora tomar uma ceva?',
+    time: 'MONDAY',
+    newMessages: '0',
+  },
+  {
+    nickname: 'Paty',
+    avatar: image5,
+    msgPreview: 'You: Nem te conto o que me aconteceu...',
+    time: 'SUNDAY',
+    newMessages: '0',
+  },
+  {
+    nickname: 'Mya',
+    avatar: image6,
+    msgPreview: 'Mya: Pois é kkkk!!',
+    time: 'SATURDAY',
+    newMessages: '0',
+  },
+]
