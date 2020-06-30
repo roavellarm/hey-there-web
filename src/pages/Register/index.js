a0a6c5b43ee580923731656545ab910dd0fbc59a
Gustavo <gustavosohne38@gmail.com>
2020-06-30 14:22:09 -0300

    register-first-done

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import Row from 'components/Row'
+import Button from 'components/Button'
+import Textfield from 'components/Textfield'
+import Column from 'components/Column'
 
 function Register() {
-  return <h1>Register</h1>
+  return (
+    <>
+      <Column size={5} style={{ padding: '100px' }}>
+        <Row>
+          <div align="center">
+            <h1>Cadastre-se</h1>
+          </div>
+
+          <Textfield style={{ width: '430px' }} label="Nome" name="nome" />
+          <br />
+          <Textfield style={{ width: '430px' }} label="Email" name="email" />
+          <br />
+          <Textfield
+            style={{ width: '430px' }}
+            type="password"
+            label="Senha"
+            name="senha"
+          />
+          <br />
+          <Button
+            color="secondary"
+            // eslint-disable-next-line no-alert
+            type="submit"
+          >
+            Cadastrar
+          </Button>
+        </Row>
+      </Column>
+    </>
+  )
 }
 
 export default Register
