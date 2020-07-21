3ebb4d9d3bc11b6f1ee4334c238438fb10ebef57
Rodrigo Avellar <roavellarm@gmail.com>
2020-07-20 23:52:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
+import GlobalStyle from 'styles/global'
 import * as serviceWorker from 'core/serviceWorker'
 import App from 'core/App'
 import theme from 'theme'
-import 'global.css'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <BrowserRouter>
       <App />
     </BrowserRouter>
