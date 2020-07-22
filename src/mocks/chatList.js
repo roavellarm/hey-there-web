4c03335c6f093f84adee2bd76db236908f001a1d
Rodrigo Avellar <roavellarm@gmail.com>
2020-07-22 19:12:20 -0300

    Fix ChatList

--- src/mocks/chatList.js
+++ src/mocks/chatList.js
@@ -7,42 +7,42 @@ import image6 from 'assets/06.jpeg'
 
 export const chatList = [
   {
-    nickname: 'Jessica dos Santos',
+    name: 'Jessica dos Santos',
     avatar: image1,
     msgPreview: 'You: Poxa vida! :(',
     time: '12:50 PM',
     newMessages: '5',
   },
   {
-    nickname: 'Maria',
+    name: 'Maria',
     avatar: image2,
     msgPreview: 'Maria: E ai gatinho? Tudo bem?',
     time: '10:03 AM',
     newMessages: '2',
   },
   {
-    nickname: 'Rebeca',
+    name: 'Rebeca',
     avatar: image3,
     msgPreview: 'You: Poxa vida! :(',
     time: '09:27 AM',
     newMessages: '0',
   },
   {
-    nickname: 'Gaby',
+    name: 'Gaby',
     avatar: image4,
     msgPreview: 'Gaby: Bora tomar uma ceva?',
     time: 'MONDAY',
     newMessages: '0',
   },
   {
-    nickname: 'Paty',
+    name: 'Paty',
     avatar: image5,
     msgPreview: 'You: Nem te conto o que me aconteceu...',
     time: 'SUNDAY',
     newMessages: '0',
   },
   {
-    nickname: 'Mya',
+    name: 'Mya',
     avatar: image6,
     msgPreview: 'Mya: Pois é kkkk!!',
     time: 'SATURDAY',
--- src/pages/ChatList/index.js
+++ src/pages/ChatList/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Row from 'components/Row'
 import Column from 'components/Column'
 import Typography from 'components/Typography'
-import Layout from 'components/Layout'
 import Avatar from 'components/Avatar'
 import { chatList } from 'mocks/chatList'
 import { StyledNumber } from './styles'
@@ -14,52 +13,51 @@ function ChatList() {
   }
 
   return (
-    <Layout>
-      <Row>
-        {chatList.map(user => {
-          return (
-            <Row
-              backgroundColor="primary"
-              radius="8px"
-              alignItems="center"
-              margin="2px"
-              onClick={handleClick}
-            >
-              <Column size={12} margin="8px 2%">
-                <Column size={1}>
-                  <Avatar src={user.avatar} />
-                </Column>
-
-                <Column size={9} alignItems="left" direction="column">
-                  <Typography size="md" weight="bold">
-                    {user.nickname}
-                  </Typography>
-                  <Typography color="secondary">{user.msgPreview}</Typography>
-                </Column>
-
-                <Column size={2} direction="column">
-                  <Typography
-                    size="sm"
-                    weight={user.newMessages !== '0' ? 'bold' : 'regular'}
-                    color={user.newMessages !== '0' ? '#32a852' : 'grey'}
-                  >
-                    {user.time}
-                  </Typography>
-
-                  {user.newMessages !== '0' ? (
-                    <StyledNumber>
-                      <Typography size="sm" weight="black">
-                        {user.newMessages}
-                      </Typography>
-                    </StyledNumber>
-                  ) : null}
-                </Column>
+    <Row>
+      {chatList.map(user => {
+        return (
+          <Row
+            key={user.name}
+            backgroundColor="primary"
+            radius="8px"
+            alignItems="center"
+            margin="2px"
+            onClick={handleClick}
+          >
+            <Column size={12} margin="8px 2%">
+              <Column size={1}>
+                <Avatar src={user.avatar} />
               </Column>
-            </Row>
-          )
-        })}
-      </Row>
-    </Layout>
+
+              <Column size={9} alignItems="left" direction="column">
+                <Typography size="md" weight="bold">
+                  {user.name}
+                </Typography>
+                <Typography color="secondary">{user.msgPreview}</Typography>
+              </Column>
+
+              <Column size={2} direction="column">
+                <Typography
+                  size="sm"
+                  weight={user.newMessages !== '0' ? 'bold' : 'regular'}
+                  color={user.newMessages !== '0' ? '#32a852' : 'grey'}
+                >
+                  {user.time}
+                </Typography>
+
+                {user.newMessages !== '0' ? (
+                  <StyledNumber>
+                    <Typography size="sm" weight="black">
+                      {user.newMessages}
+                    </Typography>
+                  </StyledNumber>
+                ) : null}
+              </Column>
+            </Column>
+          </Row>
+        )
+      })}
+    </Row>
   )
 }
 
