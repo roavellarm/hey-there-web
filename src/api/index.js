c550199a6c835b5f6b3ccb16c4b75cbbeb171e41
Rodrigo Avellar <roavellarm@gmail.com>
2020-07-24 01:51:59 -0300

    Add x-access-token to headers on requests when user is logged

--- src/api/index.js
+++ src/api/index.js
@@ -8,4 +8,32 @@ const api = axios.create({
     : 'https://heythere-staging-api.herokuapp.com',
 })
 
+api.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token')
+    if (token) return { ...config, headers: { 'x-access-token': token } }
+    return config
+  },
+  error => Promise.reject(error)
+)
+
+api.interceptors.response.use(
+  config => config,
+  error => {
+    if (error.response === undefined) {
+      const networkError = JSON.parse(JSON.stringify(error)).message
+      return Promise.reject(networkError)
+    }
+
+    const { status, data } = error.response
+    if (status === 400) return Promise.reject(data)
+    if (status === 401) {
+      localStorage.clear()
+      window.location = '/'
+      return window.location.reload()
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default api
