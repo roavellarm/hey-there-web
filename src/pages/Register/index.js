83c5feed0d66ac4cd3abd6ff990269ff9782828b
Rodrigo Avellar <roavellarm@gmail.com>
2020-07-25 02:00:45 -0300

    Update Register Layout

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import GoogleLogin from 'react-google-login'
 import Row from 'components/Row'
 import { useHistory } from 'react-router-dom'
 import Button from 'components/Button'
@@ -11,28 +12,31 @@ function Register() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
+  // const [url, setUrl] = useState('')
   const { push } = useHistory()
+  const { REACT_APP_GOOGLE_CLIENT_ID } = process.env
 
-  async function handleRegister(e) {
+  const handleRegister = async e => {
     e.preventDefault()
-
     const data = { name, email, password }
-
     const { error } = await registerService(data)
     if (error) {
       error.map(msg => showToast({ type: 'error', message: msg }))
       return null
     }
-
     push('/chat')
     return window.location.reload()
+  }
+
+  const responseGoogle = response => {
+    setEmail(response.profileObj.email)
+    // setUrl(response.profileObj.imageUrl)
   }
 
   return (
     <Column size={5}>
       <Column size={12}>
-        <h1>Cadastre-se</h1>
+        <h1>Join Hey There</h1>
       </Column>
 
       <Row>
@@ -40,21 +44,24 @@ function Register() {
           <Textfield
             value={name}
             onChange={e => setName(e.target.value)}
-            label="Nome"
-            name="nome"
+            label="Name"
+            name="name"
+            placeholder="Name"
           />
           <br />
           <Textfield
             label="Email"
             name="email"
+            placeholder="Email"
             value={email}
             onChange={e => setEmail(e.target.value)}
           />
           <br />
           <Textfield
             type="password"
-            label="Senha"
-            name="senha"
+            label="Password"
+            placeholder="Password"
+            name="password"
             value={password}
             onChange={e => setPassword(e.target.value)}
           />
@@ -66,8 +73,28 @@ function Register() {
             style={{ margin: '1rem 0px' }}
             onClick={handleRegister}
           >
-            Cadastrar
+            Sign Up
           </Button>
+          <br />
+          <GoogleLogin
+            clientId={REACT_APP_GOOGLE_CLIENT_ID}
+            render={renderProps => (
+              <Button
+                color="googleColor"
+                type="submit"
+                leftIcon="AiFillGoogleCircle"
+                fullWidth
+                style={{ margin: '1rem 0px' }}
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                Join with Google
+              </Button>
+            )}
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy="single_host_origin"
+          />
           <Toast />
         </Column>
       </Row>
