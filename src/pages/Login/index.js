27305a7bc637051175a57a45795dbc6f19f6c8a9
Rodrigo Avellar <roavellarm@gmail.com>
2020-07-24 21:40:11 -0300

    Update Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,74 +1,46 @@
 import React, { useState } from 'react'
 import GoogleLogin from 'react-google-login'
 import { useHistory } from 'react-router-dom'
-import { useStore } from 'core/store'
+import ToastAnimated, { showToast } from 'components/Toast'
 import Column from 'components/Column'
 import Textfield from 'components/Textfield'
-import Label from 'components/Label'
 import Button from 'components/Button'
 import Row from 'components/Row'
-import api from 'api/index'
+import { loginService } from 'services/authService'
 
 function Login() {
-  const { setStore } = useStore()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [url, setUrl] = useState('')
+  // const [url, setUrl] = useState('')
   const { REACT_APP_GOOGLE_CLIENT_ID } = process.env
-
   const { push } = useHistory()
 
   const responseGoogle = response => {
-    // eslint-disable-next-line no-console
-    console.log(response.profileObj)
-
     setEmail(response.profileObj.email)
-    setUrl(response.profileObj.imageUrl)
+    // setUrl(response.profileObj.imageUrl)
   }
 
   async function handleLogin(e) {
     e.preventDefault()
 
-    try {
-      const { data } = await api.post('/login', { email, password })
-
-      setStore({ token: data.token })
-      localStorage.setItem('token', data.token)
-
-      push('/chat')
-      window.location.reload()
-    } catch (error) {
-      // eslint-disable-next-line no-alert
-      alert('Erro no Login!!')
+    const { error } = await loginService({ email, password })
+    if (error) {
+      error.map(msg => showToast({ type: 'error', message: msg }))
+      return null
     }
+
+    push('/chat')
+    return window.location.reload()
   }
 
   return (
     <Column size={5}>
       <Column size={12}>
-        <h1>Login</h1>
+        <h1>LOGIN</h1>
       </Column>
 
       <Row>
         <Column size={12}>
-          <Row>
-            <Column size={12} justifyContent="center">
-              <h2>{`Welcome: ${email}`}</h2>
-              <img src={url} alt={email} />
-              <br />
-              <GoogleLogin
-                clientId={REACT_APP_GOOGLE_CLIENT_ID}
-                buttonText="Login with Google"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-                cookiePolicy="single_host_origin"
-              />
-              <br />
-              <Label> Or </Label>
-            </Column>
-          </Row>
-
-          <br />
           <Textfield
             label="Email"
             name="email"
@@ -78,8 +50,8 @@ function Login() {
           <br />
           <Textfield
             type="password"
-            label="Senha"
-            name="senha"
+            label="Password"
+            name="password"
             value={password}
             onChange={e => setPassword(e.target.value)}
           />
@@ -91,8 +63,29 @@ function Login() {
             style={{ margin: '1rem 0px' }}
             onClick={handleLogin}
           >
-            Entrar
+            Enter
           </Button>
+          <br />
+          <GoogleLogin
+            clientId={REACT_APP_GOOGLE_CLIENT_ID}
+            render={renderProps => (
+              <Button
+                color="primary"
+                type="submit"
+                leftIcon="AiFillGoogleCircle"
+                fullWidth
+                style={{ margin: '1rem 0px' }}
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                Login with Google
+              </Button>
+            )}
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy="single_host_origin"
+          />
+          <ToastAnimated />
         </Column>
       </Row>
     </Column>
