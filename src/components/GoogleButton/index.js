cdb5c82da0d2ac502d1fbf3aff69017f7e385161
Rodrigo Avellar <roavellarm@gmail.com>
2020-07-26 04:37:43 -0300

    Add GoogleButton Component

---
+++ src/components/GoogleButton/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropType from 'prop-types'
+import GoogleLogin from 'react-google-login'
+import Button from 'components/Button'
+
+function GoogleButton({ responseGoogle, text }) {
+  return (
+    <GoogleLogin
+      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+      render={renderProps => (
+        <Button
+          color="googleColor"
+          type="submit"
+          leftIcon="AiFillGoogleCircle"
+          fullWidth
+          style={{ margin: '1rem 0px' }}
+          onClick={renderProps.onClick}
+          disabled={renderProps.disabled}
+        >
+          {text}
+        </Button>
+      )}
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy="single_host_origin"
+    />
+  )
+}
+
+GoogleButton.propTypes = {
+  responseGoogle: PropType.func.isRequired,
+  text: PropType.string.isRequired,
+}
+
+export default GoogleButton
