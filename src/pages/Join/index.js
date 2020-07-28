a88022b9c97365a274a375cf9331523d78f031e9
Rodrigo Avellar <roavellarm@gmail.com>
2020-07-28 00:58:13 -0300

    Update Join

--- src/pages/Join/index.js
+++ src/pages/Join/index.js
@@ -1,40 +1,48 @@
 import React, { useState } from 'react'
 import Form from 'components/Form'
 import { useHistory } from 'react-router-dom'
+import { useStore } from 'core/store'
+import { setItem, keys } from 'helpers'
 import Toast, { showToast } from 'components/Toast'
 import { registerService } from 'services/authService'
 
 function Join() {
   const [fields, setFields] = useState({})
   const [loading, setLoading] = useState(false)
+  const { setStore } = useStore()
+  const { push } = useHistory()
 
   const handleFields = e => {
     const { name, value } = e.target
     setFields({ ...fields, [name]: value })
   }
 
-  const { push } = useHistory()
-
-  const handleJoin = async e => {
+  const handleJoin = async (params = fields) => {
     setLoading(true)
-    e.preventDefault()
-    const { error } = await registerService(fields)
+    const { data, error } = await registerService(params)
     setLoading(false)
 
     if (error) {
       typeof error === 'string'
-        ? showToast({ type: error, message: error })
+        ? showToast({ type: 'error', message: error })
         : error.map(msg => showToast({ type: 'error', message: msg }))
       return null
     }
 
+    setStore({ token: data.token })
+    setItem(keys.token, data.token)
+    setFields({ ...fields, password: '' })
     push('/chat')
-    return window.location.reload()
+    window.location.reload()
+    return null
   }
 
-  const responseGoogle = response => {
-    const { email, imageUrl: url, name } = response.profileObj
-    setFields({ ...fields, email, url, name })
+  const responseGoogle = async response => {
+    const { googleId, email, imageUrl: avatar, name } = response.profileObj
+
+    const password = `@HeyThere${googleId}`
+
+    handleJoin({ password, email, avatar, name })
   }
 
   const onKeyDown = event => {
