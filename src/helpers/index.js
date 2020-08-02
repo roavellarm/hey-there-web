9bf9be00b463d4484d663718552816f035506bdd
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-02 04:13:00 -0300

    Update Helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,25 +1,11 @@
-export const keys = {
-  token: 'token',
-  data: 'data',
-}
+export const keys = { token: 'token', data: 'data' }
 
-export const setItem = (key, data) => {
-  return localStorage.setItem(key, data)
-}
+export const setItem = (key, data) => localStorage.setItem(key, data)
 
-export const getItem = (key) => {
-  return localStorage.getItem(key)
-}
+export const getItem = (key) => localStorage.getItem(key)
 
-export const clearItems = () => {
-  localStorage.clear()
-  return window.location.reload()
-}
+export const clearItems = () => localStorage.clear() && window.location.reload()
 
-export const getCurrentUser = () => {
-  return JSON.parse(localStorage.getItem('currentUser'))
-}
+export const getCurrentUser = () => JSON.parse(getItem('currentUser'))
 
-export const setCurrentUser = (data) => {
-  return localStorage.setItem('currentUser', JSON.stringify(data))
-}
+export const setCurrentUser = (u) => setItem('currentUser', JSON.stringify(u))
