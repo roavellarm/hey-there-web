34f3ebe321abaad43003baf4e4048e90f6b39a63
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-02 04:22:07 -0300

    Add getChatInfoService and sendMessageService

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -1,8 +1,26 @@
-import { getMessagesApi } from 'api/message'
+import { getMessagesApi, sendMessagesApi, getChatInfoApi } from 'api/message'
+
+export const getChatInfoService = async (chatId) => {
+  try {
+    const data = await getChatInfoApi(chatId)
+    return data
+  } catch (error) {
+    return { error }
+  }
+}
 
 export const getMessagesService = async (chatId) => {
   try {
     const data = await getMessagesApi(chatId)
+    return data
+  } catch (error) {
+    return { error }
+  }
+}
+
+export const sendMessageService = async (chatId, message) => {
+  try {
+    const data = await sendMessagesApi(chatId, message)
     return data
   } catch (error) {
     return { error }
