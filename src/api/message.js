cbd38420d268f769e0abe9e9001b68bb77bf4372
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-02 04:10:38 -0300

    Add getChatInfoApi and sendMessagesApi

--- src/api/message.js
+++ src/api/message.js
@@ -1,3 +1,8 @@
 import api from './index'
 
+export const getChatInfoApi = (chatId) => api.get(`chat-info/${chatId}`)
+
 export const getMessagesApi = (chatId) => api.get(`chat/${chatId}`)
+
+export const sendMessagesApi = (chatId, message) =>
+  api.post(`chat/${chatId}`, message)
