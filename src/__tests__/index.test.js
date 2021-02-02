55b412b86cb0598dce78c42ffe22958279ebecbc
Rodrigo Avellar <roavellarm@gmail.com>
2021-02-02 16:26:27 -0300

    Fix Jest integration

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,7 +1,5 @@
-// import { describe, it, expect } from 'jest'
 import index from '../pages/Home/index'
 
-// eslint-disable-next-line no-undef
 describe('Should be a number', () => {
   it('should be exist', () => {
     expect(index)
