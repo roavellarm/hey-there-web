782df4930bcdd3732ab3ee847c47ab61aa4ac250
Gustavo <gustavosohne38@gmail.com>
2021-02-07 21:12:41 -0300

    first

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,7 +1,17 @@
 import index from '../pages/Home/index'
+import Login from '../pages/Login'
+// import { registerService } from '../services/authService'
 
-describe('Should be a number', () => {
-  it('should be exist', () => {
-    expect(index)
-  })
+// const freteGratis = require('./descontos').freteGratis()
+
+// test('freteGratis é verdadeiro para 200', () => {
+//   expect(freteGratis(200)).toBeTruthy()
+// })
+
+test('Expect the index exists', () => {
+  expect(index).toExist
+})
+
+test('Expect the login exists', () => {
+  expect(Login).toExist
 })
