242dba2951bf9b6e0f9fd14a53012b35e2d7e123
Gustavo <gustavo.prog40@gmail.com>
2021-08-28 12:52:48 -0300

    chat

--- src/pages/ChatRoom/index.js
+++ src/pages/ChatRoom/index.js
@@ -1,47 +1,55 @@
-/* eslint-disable react/prop-types */
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+/* eslint-disable no-unused-vars */
 import Row from 'components/Row'
+import React, { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+// import Row from 'components/Row'
 import {
   StyledMessagesContainer,
   StyledFieldArea,
   StyledButton,
-  StyledMessage,
+  // StyledMessage,
   StyledTextField,
   HoverText,
+  StyledMessage,
 } from './styles'
 
 // eslint-disable-next-line no-unused-vars
-const getMessages = id => {
-  return [
-    {
-      name: 'Rodrigo Avellar',
-      email: 'roavellarm@gmail.com',
-      message: 'Opa! Tranquilo?',
-    },
-  ]
-}
+const getMessages = [
+  {
+    name: 'Rodrigo Avellar',
+    email: 'roavellarm@gmail.com',
+    message: 'Opa! Tranquilo?',
+  },
+  {
+    name: 'Gustavo Sohne',
+    email: 'gustavo@gmail.com',
+    message: 'Eaeee! Tranquilo?',
+  },
+]
 
 const currentUser = {
   name: 'Rodrigo Avellar',
   email: 'roavellarm@gmail.com',
 }
 
-function ChatRoom({ match }) {
+function ChatRoom() {
   const [message, setMessage] = useState(undefined)
   const [chatMessages, setChatMessages] = useState([])
-  const { id } = match.params
+  // eslint-disable-next-line react/prop-types
+  const { id } = useParams()
+  console.log(id)
 
-  const getUserEmail = () => currentUser.email
+  // const getUserEmail = () => currentUser.email
 
-  const onSendMessage = async () => {
-    // try {
-    //   await sendMessage(id, message)
-    //   setMessage('')
-    // } catch (error) {
-    //   console.log(error)
-    // }
-  }
+  // const onSendMessage = async () => {
+  //   try {
+  //     await sendMessage(id, message)
+  //     setMessage('')
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+  //  612968d112b4d300047c94dc
 
   const onGetMessages = async () => {
     try {
@@ -65,26 +73,19 @@ function ChatRoom({ match }) {
           <h1>Titulo do Chat </h1>
         </HoverText>
       </Link>
-      {chatMessages.map(m => (
+      {getMessages.map(m => (
         <Row key={m.email}>
-          <StyledMessage isUser={getUserEmail() === m.email}>
+          <StyledMessage>
             <strong>
-              <div>{m.name}</div>
+              <div>{m.email}</div>
             </strong>
             <div>{m.message}</div>
           </StyledMessage>
         </Row>
       ))}
-      <StyledFieldArea>
-        <StyledTextField
-          value={message}
-          onKeyDown={({ keyCode }) => {
-            return keyCode === 13 ? onSendMessage() : undefined
-          }}
-          onChange={({ target }) => setMessage(target.value)}
-        />
-        <StyledButton onClick={() => onSendMessage()}>Send</StyledButton>
-      </StyledFieldArea>
+
+      <StyledButton onClick="only">Send</StyledButton>
+      {/* </StyledFieldArea> */}
     </StyledMessagesContainer>
   )
 }
